51feb4c4f11f4c4b99e519dee39fa59b426522cc
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-17 16:37:21 -0700

    Complete #build_graph method

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
 knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
 knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
 
-1. Put together a script that creates a game board and a knight.
-2. Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
-3. Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
+<!-- 1. Put together a script that creates a game board and a knight. -->
+<!-- 2. Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board. -->
+<!-- 3. Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series. -->
 4. Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
 
   > knight_moves([3,3],[4,3])
--- knights_travails.rb
+++ knights_travails.rb
@@ -1,25 +1,25 @@
 class Vertex
   attr_accessor \
     :coordinate,
-    :option_one,
-    :option_two,
-    :option_three,
-    :option_four,
-    :option_five,
-    :option_six,
-    :option_seven,
-    :option_eight
+    :option1,
+    :option2,
+    :option3,
+    :option4,
+    :option5,
+    :option6,
+    :option7,
+    :option8
 
-  def initialize
-    @coordinate = []
-    @option_one = nil
-    @option_two = nil
-    @option_three = nil
-    @option_four = nil
-    @option_five = nil
-    @option_six = nil
-    @option_seven = nil
-    @option_eight = nil
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @option1 = nil
+    @option2 = nil
+    @option3 = nil
+    @option4 = nil
+    @option5 = nil
+    @option6 = nil
+    @option7 = nil
+    @option8 = nil
   end
 end
 
@@ -29,22 +29,58 @@ class Graph
   def initialize
     @root = nil
   end
+
+  def build_graph(current_position, end_position, array, queue = [], values = [])
+    # take the current position, and map out moves on a BFS basis, until end_position is found
+
+    return if values.include? end_position
+
+    # add current_position to queue on first call
+    if queue.empty?
+      queue << current_position
+    end
+
+    # add current_position to list of spaces visited and remove it from queue
+    values << queue[0]
+    queue.shift
+
+    # create the root using current position
+    @root = Vertex.new(current_position)
+
+    # create all 8 possible options (if valid) and add to queue
+    current_position_moves = [[current_position[0] - 1, current_position[1] + 2],
+                              [current_position[0] + 1, current_position[1] + 2],
+                              [current_position[0] + 2, current_position[1] + 1],
+                              [current_position[0] + 2, current_position[1] - 1],
+                              [current_position[0] + 1, current_position[1] - 2],
+                              [current_position[0] - 1, current_position[1] - 2],
+                              [current_position[0] - 2, current_position[1] - 1],
+                              [current_position[0] - 2, current_position[1] + 1]]
+
+    # add possible moves to queue if they're within the board
+    8.times do |x|
+      if array.include? current_position_moves[x] then queue << current_position_moves[x] end
+    end
+
+    # call build_graph on first item in queue
+    build_graph(queue[0], end_position, array, queue, values)
+
+    # return values array
+    values
+  end
 end
 
 class Knight
-  attr_accessor :current_location
+  attr_accessor
 
   def initialize
-    # @current_location
+    @game_board = GameBoard.new
   end
 
-  def all_moves
-    # method to create a graph of all possible moves
-  end
-
-  def knight_moves
+  def knight_moves(current_position, end_position)
     # Use the chosen search algorithm to find the shortest path between
     # the starting square (or node) and the ending square.
+    moves_graph = Graph.new.build_graph(current_position, end_position, @game_board.board)
   end
 end
 
@@ -68,5 +104,4 @@ class GameBoard
 end
 
 new_knight = Knight.new
-new_board = GameBoard.new
-p new_board.board
+p new_knight.knight_moves([3, 5], [7, 7])
